865f9259458d497d29806ab462c36ef5ddaf8dff
Deepak <er.d.ahuja@gmail.com>
2018-07-11 17:03:14 +0530

    fix: add the lazy ll

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { getProducts } from '../Client'
+import { getProducts } from '../Client';
+import Product from './Product.js';
+import Waypoint from 'react-waypoint';// http://brigade.github.io/react-waypoint/#infinite-scroll
 class ProductList extends Component {
   constructor() {
     super();
     this.state = {
-      fetched: true,
+      isLoading: true,
       products: []
     }
   }
@@ -15,13 +17,36 @@ class ProductList extends Component {
     const { products } = await getProducts();
     this.setState({
         products,
-        fetched: true,
+        isLoading: true,
     })
   };
+  _renderItems = () => {
+    return this.state.products.map(function(product, index) {
+      return (
+        <Product
+          // src={}
+          // alt="CATS AND ROBOTS... "
+          // style={{ height: 480 }}
+          // key={index}
+          className="infinite-scroll-example__list-item" />
+      );
+    });
+  }
+  _renderWaypoint = () => {
+    if (!this.state.isLoading) {
+      return (
+        <Waypoint
+          onEnter={this._loadMoreItems}
+        />
+      );
+    }
+  }
   render() {
     return (
       <div>
-        
+         {this._renderItems()}
+        {this._renderWaypoint()}
+        Loading more items…
       </div>
     );
   }
